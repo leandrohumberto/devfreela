57f64772ae48a85cb8df2b801e691c72396f94bb
Leandro Humberto Melo dos Santos <lehumberto@msn.com>
2022-09-12 21:42:12 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=net core GET
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        // api/projects/1 GET
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // return NotFound();
+            return Ok();
+        }
+
+        // api/projects POST
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProjectModel)
+        {
+            if (createProjectModel.Title?.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            // Cadastro o projeto
+
+            return CreatedAtAction(nameof(GetById), new { id = createProjectModel.Id }, createProjectModel);
+        }
+
+        // api/projects/1 PUT
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProjectModel)
+        {
+            if (updateProjectModel.Description?.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            // Atualizo o projeto
+
+            return NoContent();
+        }
+
+        // api/projects/1 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar; se não existir, retorna NotFound
+
+            // Remover
+
+            return NoContent();
+        }
+    }
+}
